dfdac54b24b45491956da753bdac1d1b25348dc6
cansoking <cansoking@hotmail.com>
2023-09-05 09:22:53 +0800

    优化网站图标和标题细节

--- src/components/sitepart/SatHead.vue
+++ src/components/sitepart/SatHead.vue
@@ -2,17 +2,16 @@
   <div>
     <el-header style="background-color: #222831">
       <!-- Header content -->
-      <el-menu
-        class="el-menu-demo"
-        mode="horizontal"
-        background-color="#222831"
-        text-color="#eeeeee"
-        active-text-color="#00adb5"
-      >
-        <el-menu-item index="1">
-          <i style="font-size: 30px" class="el-icon-odometer"></i>
-          卫星认证控制中心
-        </el-menu-item>
+      <el-menu class="el-menu-demo" mode="horizontal" background-color="#222831" text-color="#eeeeee"
+        active-text-color="#00adb5">
+        <el-row :gutter="0" type="flex" align="middle">
+          <el-col :span="0.5" :offset="0">
+            <el-image :src="logo_img_url" fit="fill" style="width: 45px;"></el-image>
+          </el-col>
+          <el-col style="line-height: 60px; margin-left: 10px;" :span="12" :offset="0">
+            <span style="color: white;">卫星认证控制中心</span>
+          </el-col>
+        </el-row>
       </el-menu>
     </el-header>
   </div>
@@ -23,14 +22,15 @@ export default {
   name: "SatAuthSatHead",
 
   data() {
-    return {};
+    return {
+      logo_img_url: require("../../assets/logo.png")
+    };
   },
 
-  mounted() {},
+  mounted() { },
 
   methods: {},
 };
 </script>
 
-<style>
-</style>
+<style></style>
